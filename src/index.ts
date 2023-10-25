74938287f8bba2230751824b88cd9d7240a071ea
Jeppe Christiansen <jeppe@codr.dk>
2023-10-25 14:03:52 +0200

    formatting

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,45 @@
-type EventCallback<T> = (data: T) => void;
-type RemoveListener = () => void;
+type EventCallback<T> = (data: T) => void
+type RemoveListener = () => void
 
 export class TypedEmitter<Events> {
-  private listeners: any[] = [];
-  private listeners_once: any[] = [];
+
+  private listeners: any[] = []
+  private listeners_once: any[] = []
 
   public on<Event extends keyof Events>(event: Event, fn: EventCallback<Events[Event]>): RemoveListener {
-    this.listeners.push({ event, fn });
-    return () => this.remove_listener(event, fn);
+    this.listeners.push({ event, fn })
+    return () => this.remove_listener(event, fn)
   }
 
   public once<Event extends keyof Events>(event: Event, fn: EventCallback<Events[Event]>): RemoveListener {
-    this.listeners_once.push({ event, fn });
-    return () => this.remove_listener(event, fn);
+    this.listeners_once.push({ event, fn })
+    return () => this.remove_listener(event, fn)
   }
 
   public off<Event extends keyof Events>(event: Event, fn: EventCallback<Events[Event]>) {
-    this.remove_listener(event, fn);
+    this.remove_listener(event, fn)
   }
 
   public emit<Event extends keyof Events>(event: Event, data?: Events[Event]) {
     for (const listener of this.listeners) {
+      
       if (listener.event === event) {
-        listener.fn(data);
+        listener.fn(data)
       }
     }
 
     this.listeners_once = this.listeners_once.filter((listener) => {
       if (listener.event === event) {
-        listener.fn(data);
-        return false;
+        listener.fn(data)
+        return false
       }
-      return true;
-    });
+      return true
+    })
   }
 
   private remove_listener<Event extends keyof Events>(event: Event, fn: EventCallback<Events[Event]>) {
     this.listeners = this.listeners.filter((listener) => {
-      return listener.event !== event && listener.fn !== fn;
-    });
+      return (listener.event !== event && listener.fn !== fn)
+    })
   }
 }
